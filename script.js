77d167a9940683332f6d11555b3ec6fc69a346ac
rapirawr <rabdillahf09@gmai.com>
2025-10-21 13:50:19 +0700

    a

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const changeTheme = document.getElementById("changeTheme");
+
+changeTheme.addEventListener('click', function(){
+    document.body.classList.toggle("dark-mode")
+    changeTheme.classList.toggle("active")
+});
+
 function sapaSaya() {
     alert("Halo, Rafi Abdillah Fairuz! Semoga harimu menyenangkan!");
-}
+};
